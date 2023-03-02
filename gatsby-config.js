82462b84f57be74bb05199f7703ac799d5c336d4
Abhishek Sharma <abhi.asno1@gmail.com>
2023-03-02 16:58:09 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag"]
+};
